e51d1ecaad03ac77d7555e4021e5cc4d6f9ec611
hirekaan.hemen <hirekaan.hemen@konga.com>
2023-03-07 18:32:38 +0100

    Implemented Event Sourcing

--- SM-POST/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM-POST/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,13 +1,15 @@
 using CQRS.Core.Domain;
-
+using CQRS.Core.Infra;
 using Post.Cmd.Infra.Config;
 using Post.Cmd.Infra.Repositories;
+using Post.Cmd.Infra.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
+builder.Services.AddSingleton<IEventStore, EventStore>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ SM-POST/Post.Cmd/Post.Cmd.Infra/Handlers/EventSourcingHandler.cs
@@ -0,0 +1,40 @@
+
+
+using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infra;
+using Post.Cmd.Domain;
+
+namespace Post.Cmd.Infra.Handlers
+{
+
+    public class EventSourcingHandler : IEventSourcingHandler<PostAggregate>
+    {
+        private readonly IEventStore _eventStore;
+
+        public EventSourcingHandler(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        public async Task<PostAggregate> GetByIdAsync(Guid Id)
+        {
+            PostAggregate postAggregate = new();
+            var events = await _eventStore.GetEventsAsync(Id);
+
+            if (events != null && events.Any())
+            {
+                postAggregate.ReplayEvents(events);
+                postAggregate.Version = events.Select(x => x.Version).Max();
+            }
+
+            return postAggregate;
+        }
+
+        public async Task SaveAsync(AggregateRoot aggregateRoot)
+        {
+            await _eventStore.SaveEventsAsync(aggregateRoot.Id, aggregateRoot.GetUncommitedChanges(), aggregateRoot.Version);
+            aggregateRoot.MockChangesAsCommited();
+        }
+    }
+}
